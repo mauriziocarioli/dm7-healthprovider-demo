aadec28e7a093d495589571ec95b873e9b6baaaa
dmAdmin <>
2019-08-06 10:03:36 -0400

    {/src/main/java/com/myspace/spectrum/Appointment.java}

--- src/main/java/com/myspace/spectrum/Appointment.java
+++ src/main/java/com/myspace/spectrum/Appointment.java
@@ -6,12 +6,33 @@ package com.myspace.spectrum;
 
 public class Appointment implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Appointment() {
-    }
+	private java.lang.Boolean scheduled;
+	private java.util.Date date;
 
+	public Appointment() {
+	}
 
+	public java.lang.Boolean getScheduled() {
+		return this.scheduled;
+	}
 
+	public void setScheduled(java.lang.Boolean scheduled) {
+		this.scheduled = scheduled;
+	}
+
+	public java.util.Date getDate() {
+		return this.date;
+	}
+
+	public void setDate(java.util.Date date) {
+		this.date = date;
+	}
+
+	public Appointment(java.lang.Boolean scheduled, java.util.Date date) {
+		this.scheduled = scheduled;
+		this.date = date;
+	}
 
 }
